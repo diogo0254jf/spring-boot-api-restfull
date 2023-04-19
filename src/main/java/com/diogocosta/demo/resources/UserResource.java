884d4ce6c9423d13cccd622c46d7ca0232607fc4
maria menezes <diogo0254jf@gmail.com>
2023-04-19 12:04:03 -0300

    DTO user return

--- src/main/java/com/diogocosta/demo/resources/UserResource.java
+++ src/main/java/com/diogocosta/demo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.diogocosta.demo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.diogocosta.demo.domain.User;
+import com.diogocosta.demo.dto.UserDTO;
 import com.diogocosta.demo.services.UserServices;
 
 @RestController
@@ -18,10 +20,10 @@ public class UserResource {
     private UserServices service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
-    
 
 }
